ba0d8dfba626e49f5ebb9d2aa82ab87127cb164f
Patryk <patryk.podlas@outlook.com>
2022-02-28 15:41:15 +0000

    updated

--- Get-vSphereCDP.ps1
+++ Get-vSphereCDP.ps1
@@ -1,59 +1,71 @@
-function Get-VMwareCDP {
+function Get-vSphereCDP {
     <#
     .SYNOPSIS
         Simple function to quickly retrieve the CDP information for any cluster or host.
     .DESCRIPTION
         Simple function to quickly retrieve the CDP information for any cluster or host. This can be useful to send to the networking team through a readable format without manually copying/pasting the information through the GUI, for the purpose of tagging the switchports with needed VLANs.
         The function works with | Export-CSV -Path and is the recommended approach.
+    .PARAMETER Clusters
+        Specify one or more comma-separated cluster names.
+    .PARAMETER ESXiHost
+        Specify a single ESXi hostname.
     .EXAMPLE
-        PS C:\> Get-VMwareCDP -Cluster <name> | Format-Table
+        PS C:\> Get-vSphereCDP | Format-Table
+        Run the without any parameters to cycle through every cluster the VCSA sees.
+    .EXAMPLE
+        PS C:\> Get-vSphereCDP -Clusters WK-92-NP-LAB | Format-Table
         Specifying the cluster will go through each host within and retrieve the CDP information and formats the output in a table format.
     .EXAMPLE
-        PS C:\> Get-VMwareCDP -ESXiHost <name> | Out-GridView
+        PS C:\> Get-vSphereCDP -ESXiHost ESX-UK-WK-92-01 | Out-GridView
         Specyfing the host will only target the host during CDP retrieval and displays a table with the results. The table can then be sorted as desired.
     .EXAMPLE
-        PS C:\> Get-VMwareCDP -Cluster <name> | Export-CSV -Path .\<name>.csv -NoTypeInformation
+        PS C:\> Get-vSphereCDP -Clusters WK-92-NP-LAB | Export-CSV -Path .\<filename>.csv -NoTypeInformation
         Retrieves the CDP information for the entire cluster and exports the results to a CSV file without type information.
     .NOTES
         Author: Patryk Podlas
-        Created: January 2022
-        Change History:
+        Created: February 2022
+
+        Change history:
+        Date            Author      V       Notes
+        17/02/2022      PP          1.0     First release
     #>
-    [CmdletBinding()]
+    #Requires -Modules VMware.VimAutomation.Core
+    #Requires -Version 5.1
+    [CmdletBinding(DefaultParameterSetName = "Clusters")]
     param (
-        [Parameter(Mandatory, ParameterSetName = "Cluster")]
-        [string]$Cluster,
-        [Parameter(Mandatory, ParameterSetName = "ESXiHost")]
+        [Parameter(ParameterSetName = "Clusters")]
+        [string[]]$Clusters = (Get-Cluster),
+        [Parameter(ParameterSetName = "ESXiHost")]
         [string]$ESXiHost
     )
 
     begin {
-        if (!$global:DefaultVIServer) {
-            Write-Error -Message "Not Connected to any vCenters.  Connect to all linked vcenters (Connect-VIServer <vCenter> -AllLinked) before running this command ..." -Category AuthenticationError -ErrorAction Stop
-        }
+
     }
 
     process {
-        if ($Cluster) {
-            $VMHosts = Get-Cluster -Name $Cluster | Get-VMHost
-            $returnObj = @()
-            foreach ($VMHost in $VMHosts) {
-                $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
-                foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
-                    $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
-                    $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
-                    $Obj = [PSCustomObject] @{
-                        'Host'       = $VMHost.Name
-                        'VMNIC'      = $Pnic.Device
-                        'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
-                        'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
-                        'MAC'        = $Pnic.Mac
-                        'Driver'     = $Pnic.Driver
-                        'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
+        if ($Clusters) {
+            foreach ($Cluster in $Clusters) {
+                $returnObj = @()
+                foreach ($VMHost in Get-Cluster -Name $Cluster | Get-VMHost) {
+                    $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
+                    foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
+                        $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
+                        $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
+
+                        $Obj = [PSCustomObject] @{
+                            'Host'       = $VMHost.Name
+                            'VMNIC'      = $Pnic.Device
+                            'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
+                            'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
+                            'MAC'        = $Pnic.Mac
+                            'Driver'     = $Pnic.Driver
+                            'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
+                        }
+                        $returnObj += $Obj
                     }
-                    $returnObj += $Obj
                 }
-            }   $returnObj
+            }
         }
         if ($ESXiHost) {
             $VMHost = Get-VMHost -Name $($ESXiHost + "*")
