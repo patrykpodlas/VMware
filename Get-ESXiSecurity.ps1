2a76f316d70ccc3f7e7ebf9c5c5742f68aff5886
Patryk <patryk.podlas@outlook.com>
2022-07-14 09:45:43 +0100

    changes and upload

---
+++ Get-ESXiSecurity.ps1
@@ -0,0 +1,163 @@
+function Get-ESXiSecurity {
+    <#
+    .SYNOPSIS
+        Function which retrieves current security values and services status set on each ESXi host.
+    .DESCRIPTION
+        Function which retrieves current security values and services status set on each ESXi host.
+        Each value is examined based on security standards, if the value matches then the result is a success, otherwise it's a fail, output shows what the value should be and what it currently is set to.
+        The results are exported to HTML format into $env:USERPROFILE\Documents\TEMP, as well as outputted immediately to Out-GridView.
+        The function will target all hosts added to the VCSA currently connected to.
+        If no parameters are specified, all parameters are used by default.
+    .PARAMETER Settings
+        Retrieve the ESXi server security settings.
+    .PARAMETER Services
+        Retrieve the ESXi server services settings.
+    .EXAMPLE
+        PS C:\> Get-ESXiSecurity -Settings -Services
+        Exports both, the settings and services statuses, this action is done by default.
+    .NOTES
+        Author: Patryk Podlas
+        Created: January 2022
+
+        Change history:
+        Date            Author      V       Notes
+        01/02/2022      PP          1.0     First release
+    #>
+    #Requires -Modules VMware.VimAutomation.Core
+    #Requires -Version 5.1
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [switch]$Settings,
+        [Parameter()]
+        [switch]$Services
+    )
+
+    begin {
+        $ESXiHosts = @(
+            Get-VMHost | Select-Object Name
+        )
+        if (!$Settings -and !$Services) {
+            $Settings = $true
+            $Services = $true
+        }
+        #HTML formatting for the export.
+        $Header = @"
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+    <head>
+    <title>ESXi report</title>
+    <style type="text/css">
+    <!--
+    body {
+    background-color: #E0E0E0;
+    font-family: open-sans
+    }
+    table, th, td {
+    background-color: white;
+    border-collapse:collapse;
+    border: 1px solid green;
+    padding: 5px
+    }
+    -->
+    </style>
+"@
+        $Body = @"
+    <h1>ESXi report</h1>
+"@
+
+    }
+
+    process {
+        if ($Settings) {
+            #ESXi settings which are based on the defined standard.
+            $ESXiSettings = @{
+                "Security.AccountUnlockTime"              = 900
+                "Security.AccountLockFailures"            = 3
+                "Security.PasswordHistory"                = 5
+                "UserVars.DcuiTimeOut"                    = 600
+                "UserVars.ESXiShellTimeOut"               = 600
+                "UserVars.ESXiShellInteractiveTimeOut"    = 600
+                "UserVars.SuppressShellWarning"           = 0
+                #"Config.HostAgent.plugins.hostsvc.esxAdminsGroup" = "domain\accountorgroup"
+                "Syslog.global.logDir"                    = "[] /scratch/log" #Logging directory, need to establish what it should be.
+                "Syslog.global.logHost"                   = $true #Logging enabled
+                "UserVars.ESXiVPsDisabledProtocols"       = "sslv3,tlsv1,tlsv1.1"
+                "Mem.ShareForceSalting"                   = 2
+                "Config.HostAgent.plugins.solo.enableMob" = $false
+            }
+            #Evaluates the ESXi settings.
+            $returnObj = @()
+            foreach ($ESXiHost in $ESXiHosts.Name) {
+                foreach ($Setting in $ESXiSettings.GetEnumerator()) {
+                    $SettingCheck = Get-AdvancedSetting -Entity $ESXiHost | Where-Object -Property Name -EQ $Setting.Name
+                    if ($SettingCheck.Value -EQ $Setting.Value) {
+                        $PassOrFail = 'PASS'
+                        $Reason = $null
+                    } elseif (($SettingCheck.Value -NE $Setting.Value) -or (!$SettingCheck.Value)) {
+                        $PassOrFail = 'FAIL'
+                        if (!$SettingCheck.Value) {
+                            $Reason = "NULL ≠ $($Setting.Value)"
+                        } else {
+                            $Reason = "$($SettingCheck.Value) ≠ $($Setting.Value)"
+                        }
+                    }
+                    $Obj = New-Object psobject -Property @{
+                        "Test"               = $Setting.Name
+                        "Pass or Fail"       = $PassOrFail
+                        "ESXi"               = $ESXiHost
+                        "Reason for failure" = $Reason
+                        "Value"              = $SettingCheck.Value
+                    }
+                    $returnObj += $Obj | Select-Object -Property 'Pass or Fail', Test, ESXi, 'Reason For Failure', Value
+                }
+            }
+
+            $returnObj | Sort-Object -Property 'Pass or Fail' | ConvertTo-Html -Head $Header -Body $Body | ForEach-Object {
+                $_ -replace "<td>FAIL</td>", "<td style='background-color:#FF0000'>FAIL</td>" -replace "<td>PASS</td>", "<td style='background-color:#008000'>PASS</td>"
+            } | Out-File -FilePath $env:USERPROFILE\Documents\TEMP\ESXiReport_settings.html ; Write-Output "Results are exported to $env:USERPROFILE\Documents\TEMP\ESXiReport_settings.html`n"
+            $returnObj | Out-GridView -Title "ESXi Settings"
+        }
+        if ($Services) {
+            #ESXi services which are based on the defined standard.
+            $ESXiServices = @{
+                "ntpd"           = "on" #NTP Enabled
+                "sfcbd-watchdog" = "off" #CIM Disabled
+                "slpd"           = "off" #SLP Disabled
+                "snmpd"          = "off" #SNMP Disabled
+                "TSM-SSH"        = "off" #SSH Disabled
+                "TSM"            = "off" #ESXiShell Disabled
+            }
+            #Evaluates the ESXi services.
+            $returnObj = @()
+            foreach ($ESXiHost in $ESXiHosts.Name) {
+                foreach ($Setting in $ESXiServices.GetEnumerator()) {
+                    $SettingCheck = Get-VMHost -Name $ESXiHost | Get-VMHostService | Where-Object -Property Key -EQ $Setting.Name
+                    if ($SettingCheck.Policy -EQ $Setting.Value) {
+                        $PassOrFail = 'PASS'
+                        $Reason = $null
+                    } elseif ($SettingCheck.Policy -NE $Setting.Value) {
+                        $PassOrFail = 'FAIL'
+                        $Reason = "$($SettingCheck.Policy) ≠ $($Setting.Value)"
+                    }
+                    $Obj = New-Object psobject -Property @{
+                        "Test"               = $Setting.Name
+                        "Pass or Fail"       = $PassOrFail
+                        "ESXi"               = $ESXiHost
+                        "Reason for failure" = $Reason
+                        "Value"              = $SettingCheck.Policy
+                    }
+                    $returnObj += $Obj | Select-Object -Property 'Pass or Fail', Test, ESXi, 'Reason For Failure', Value
+                }
+            }
+            $returnObj | Sort-Object -Property 'Pass or Fail' | ConvertTo-Html -Head $Header -Body $Body | ForEach-Object {
+                $_ -replace "<td>FAIL</td>", "<td style='background-color:#FF0000'>FAIL</td>" -replace "<td>PASS</td>", "<td style='background-color:#008000'>PASS</td>"
+            } | Out-File -FilePath $env:USERPROFILE\Documents\TEMP\ESXiReport_Services.html ; Write-Output "Results are exported to $env:USERPROFILE\Documents\TEMP\ESXiReport_services.html`n"
+            $returnObj | Out-GridView -Title "ESXi Services"
+        }
+    }
+
+    end {
+
+    }
+}
--- Get-vSphereCDP.ps1
+++ Get-vSphereCDP.ps1
@@ -1,6 +1,4 @@
-#please-sign-me
-function Get-vSphereCDP {
-    <#
+<#
     .SYNOPSIS
         Simple function to quickly retrieve the CDP information for any cluster or host.
     .DESCRIPTION
@@ -14,13 +12,13 @@ function Get-vSphereCDP {
         PS C:\> Get-vSphereCDP | Format-Table
         Run the without any parameters to cycle through every cluster the VCSA sees.
     .EXAMPLE
-        PS C:\> Get-vSphereCDP -Clusters <ClusterName> | Format-Table
+        PS C:\> Get-vSphereCDP -Clusters WK-92-NP-LAB | Format-Table
         Specifying the cluster will go through each host within and retrieve the CDP information and formats the output in a table format.
     .EXAMPLE
-        PS C:\> Get-vSphereCDP -ESXiHost <ClusterName> | Out-GridView
+        PS C:\> Get-vSphereCDP -ESXiHost ESX-UK-WK-92-01 | Out-GridView
         Specyfing the host will only target the host during CDP retrieval and displays a table with the results. The table can then be sorted as desired.
     .EXAMPLE
-        PS C:\> Get-vSphereCDP -Clusters <ClusterName> | Export-CSV -Path .\CDPInfoFor<ClusterName>.csv -NoTypeInformation
+        PS C:\> Get-vSphereCDP -Clusters WK-92-NP-LAB | Export-CSV -Path .\<filename>.csv -NoTypeInformation
         Retrieves the CDP information for the entire cluster and exports the results to a CSV file without type information.
     .NOTES
         Author: Patryk Podlas
@@ -29,69 +27,70 @@ function Get-vSphereCDP {
         Change history:
         Date            Author      V       Notes
         17/02/2022      PP          1.0     First release
-        18/02/2022      PP          1.1     Added multiple cluster support and default value to all clusters
     #>
-    #Requires -Modules VMware.VimAutomation.Core
-    #Requires -Version 5.1
-    [CmdletBinding(DefaultParameterSetName = "Clusters")]
-    param (
-        [Parameter(ParameterSetName = "Clusters")]
-        [string[]]$Clusters = (Get-Cluster),
-        [Parameter(ParameterSetName = "ESXiHost")]
-        [string]$ESXiHost
-    )
+#Requires -Modules VMware.VimAutomation.Core
+#Requires -Version 5.1
+[CmdletBinding(DefaultParameterSetName = "Clusters")]
+param (
+    [Parameter(ParameterSetName = "Clusters")]
+    [string[]]$Clusters = (Get-Cluster),
+    [Parameter(ParameterSetName = "ESXiHost")]
+    [string]$ESXiHost
+)
 
-    begin {
+begin {
 
-    }
+}
 
-    process {
-        if ($Clusters) {
-            foreach ($Cluster in $Clusters) {
-                $returnObj = @()
-                foreach ($VMHost in Get-Cluster -Name $Cluster | Get-VMHost) {
-                    $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
-                    foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
-                        $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
-                        $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
+process {
+    if ($Clusters) {
+        foreach ($Cluster in $Clusters) {
+            $returnObj = @()
+            foreach ($VMHost in Get-Cluster -Name $Cluster | Get-VMHost) {
+                $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
+                foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
+                    $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
+                    $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
 
-                        $Obj = [PSCustomObject] @{
-                            'Host'       = $VMHost.Name
-                            'VMNIC'      = $Pnic.Device
-                            'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
-                            'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
-                            'MAC'        = $Pnic.Mac
-                            'Driver'     = $Pnic.Driver
-                            'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
-                        }
-                        $returnObj += $Obj
+                    $Obj = [PSCustomObject] @{
+                        'Host'       = $VMHost.Name
+                        'VMNIC'      = $Pnic.Device
+                        'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
+                        'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
+                        'MAC'        = $Pnic.Mac
+                        'Driver'     = $Pnic.Driver
+                        'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
                     }
+                    $returnObj += $Obj
                 }
             }
         }
-        if ($ESXiHost) {
-            $VMHost = Get-VMHost -Name $($ESXiHost + "*")
-            $returnObj = @()
-            $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
-            foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
-                $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
-                $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
-
-                $Obj = [PSCustomObject] @{
-                    'Host'       = $VMHost.Name
-                    'VMNIC'      = $Pnic.Device
-                    'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
-                    'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
-                    'MAC'        = $Pnic.Mac
-                    'Driver'     = $Pnic.Driver
-                    'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
-                }
-                $returnObj += $Obj
-            }
-        }   $returnObj
     }
+    if ($ESXiHost) {
+        $VMHost = Get-VMHost -Name $($ESXiHost + "*")
+        $returnObj = @()
+        $NetSystem = Get-View $VMHost.ExtensionData.ConfigManager.NetworkSystem
+        foreach ($Pnic in $VMHost.ExtensionData.Config.Network.Pnic) {
+            $PnicInfo = $NetSystem.QueryNetworkHint($Pnic.Device)
+            $Speed = $VMHost | Get-VMHostNetworkAdapter -Name $Pnic.Device | Select-Object BitRatePerSec
 
-    end {
+            $Obj = [PSCustomObject] @{
+                'Host'       = $VMHost.Name
+                'VMNIC'      = $Pnic.Device
+                'Switch'     = $PnicInfo.ConnectedSwitchPort.DevId
+                'SwitchPort' = $PnicInfo.ConnectedSwitchPort.PortId
+                'MAC'        = $Pnic.Mac
+                'Driver'     = $Pnic.Driver
+                'Speed(GB)'  = $($Speed.BitRatePerSec) / 1000
+            }
+            $returnObj += $Obj
+        }
+    }   $returnObj
+}
 
+end {
+
+}
+}
     }
 }
