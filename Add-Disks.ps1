c0dfbb6e04e8d63198f45056b81fc0e88373f734
Patryk <patryk.podlas@outlook.com>
2023-02-08 17:45:36 +0000

    update

--- Add-Disks.ps1
+++ Add-Disks.ps1
@@ -113,11 +113,11 @@ function Add-Disks {
 
         # Add temporary disks with the controllers
         Write-Output "---Adding SCSI controllers."
-        $VM | New-HardDisk -CapacityGB 1 | New-ScsiController -Type ParaVirtual
+        $VM | New-HardDisk -CapacityGB 1 | New-ScsiController -Type ParaVirtual | Out-Null
         Start-Sleep 5
-        $VM | New-HardDisk -CapacityGB 2 | New-ScsiController -Type ParaVirtual
+        $VM | New-HardDisk -CapacityGB 2 | New-ScsiController -Type ParaVirtual | Out-Null
         Start-Sleep 5
-        $VM | New-HardDisk -CapacityGB 3 | New-ScsiController -Type ParaVirtual
+        $VM | New-HardDisk -CapacityGB 3 | New-ScsiController -Type ParaVirtual | Out-Null
         Start-Sleep 5
 
         # Power on the VM and remove the temporary disks - this is necessary because if the VM is powered off, it will remove the SCSI controllers as well.
@@ -132,7 +132,6 @@ function Add-Disks {
         $VM | Stop-VM -Confirm:$false | Out-Null
 
         # Configure the correct order of the SCSI controllers.
-        # New slot number to use (number, not a string -- no quotes)
         Write-Output "---Reconfiguring the controllers with appropriate SCSI slot numbers."
         $intNewSlotNumber = 1184
         $scsi0NewSlotNumber = 160
@@ -140,88 +139,57 @@ function Add-Disks {
         $scsi2NewSlotNumber = 224
         $scsi3NewSlotNumber = 256
 
-
         # Network
-        ## create new VMConfigSpec object
         $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-        ## create an array of OptionValues (1 item long)
         $spec.extraConfig = New-Object VMware.Vim.OptionValue[] (1)
-        ## create a new OptionValue with the desired values
         $spec.extraConfig[0] = New-Object VMware.Vim.OptionValue -Property @{
             key   = "ethernet0.pciSlotNumber"
             value = $intNewSlotNumber
-        } ## end new-object
-
-        ## get the .NET View object for the VM
+        }
         $viewVMToReconfig = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Name" = $VMName }
-        ## reconfig the VM with the new spec
         $viewVMToReconfig.ReconfigVM_Task($spec) | Out-Null
 
         # SCSI 0
-        ## create new VMConfigSpec object
         $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-        ## create an array of OptionValues (1 item long)
         $spec.extraConfig = New-Object VMware.Vim.OptionValue[] (1)
-        ## create a new OptionValue with the desired values
         $spec.extraConfig[0] = New-Object VMware.Vim.OptionValue -Property @{
             key   = "scsi0.pciSlotNumber"
             value = $scsi0NewSlotNumber
-        } ## end new-object
-
-        ## get the .NET View object for the VM
+        }
         $viewVMToReconfig = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Name" = $VMName }
-        ## reconfig the VM with the new spec
         $viewVMToReconfig.ReconfigVM_Task($spec) | Out-Null
         Start-Sleep 5
 
         # SCSI 1
-        ## create new VMConfigSpec object
         $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-        ## create an array of OptionValues (1 item long)
         $spec.extraConfig = New-Object VMware.Vim.OptionValue[] (1)
-        ## create a new OptionValue with the desired values
         $spec.extraConfig[0] = New-Object VMware.Vim.OptionValue -Property @{
             key   = "scsi1.pciSlotNumber"
             value = $scsi1NewSlotNumber
-        } ## end new-object
-
-        ## get the .NET View object for the VM
+        }
         $viewVMToReconfig = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Name" = $VMName }
-        ## reconfig the VM with the new spec
         $viewVMToReconfig.ReconfigVM_Task($spec) | Out-Null
         Start-Sleep 5
 
         # SCSI 2
-        ## create new VMConfigSpec object
         $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-        ## create an array of OptionValues (1 item long)
         $spec.extraConfig = New-Object VMware.Vim.OptionValue[] (1)
-        ## create a new OptionValue with the desired values
         $spec.extraConfig[0] = New-Object VMware.Vim.OptionValue -Property @{
             key   = "scsi2.pciSlotNumber"
             value = $scsi2NewSlotNumber
-        } ## end new-object
-
-        ## get the .NET View object for the VM
+        }
         $viewVMToReconfig = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Name" = $VMName }
-        ## reconfig the VM with the new spec
         $viewVMToReconfig.ReconfigVM_Task($spec) | Out-Null
         Start-Sleep 5
 
         # SCSI 3
-        ## create new VMConfigSpec object
         $spec = New-Object VMware.Vim.VirtualMachineConfigSpec
-        ## create an array of OptionValues (1 item long)
         $spec.extraConfig = New-Object VMware.Vim.OptionValue[] (1)
-        ## create a new OptionValue with the desired values
         $spec.extraConfig[0] = New-Object VMware.Vim.OptionValue -Property @{
             key   = "scsi3.pciSlotNumber"
             value = $scsi3NewSlotNumber
-        } ## end new-object
-
-        ## get the .NET View object for the VM
+        }
         $viewVMToReconfig = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Name" = $VMName }
-        ## reconfig the VM with the new spec
         $viewVMToReconfig.ReconfigVM_Task($spec) | Out-Null
         Start-Sleep 5
     }
